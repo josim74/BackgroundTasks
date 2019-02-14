73ab8516cde308df050d47526e974f663bc7f6ff
Josim Uddin <mdjosimuddin712@gmail.com>
2019-02-14 14:25:34 +0600

    Initial commit

---
+++ app/src/main/java/com/github/josim74/backgroundtasks/service/ServiceHomeActivity.kt
@@ -0,0 +1,13 @@
+ package com.github.josim74.backgroundtasks.service
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.github.josim74.backgroundtasks.R
+
+ class ServiceHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service_home)
+    }
+}
